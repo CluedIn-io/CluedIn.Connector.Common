de497e1a70f5349ce68d5311fbad0d72e8bdec1e
Ben Fidge <ben.fidge@btopenworld.com>
2022-01-24 08:12:11 +0700

    For AB#7135

--- src/Connector.Common/Configurations/CommonConfigurationNames.cs
+++ src/Connector.Common/Configurations/CommonConfigurationNames.cs
@@ -1,12 +1,37 @@
-﻿namespace CluedIn.Connector.Common.Configurations
+﻿using RestSharp.Extensions;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+
+namespace CluedIn.Connector.Common.Configurations
 {
+
     public static class CommonConfigurationNames
     {
-        public const string Host = nameof(Host);
-        public const string DatabaseName = nameof(DatabaseName);
-        public const string Username = nameof(Username);
-        public const string Password = nameof(Password);
-        public const string PortNumber = nameof(PortNumber);
-        public const string Schema = nameof(Schema);
+        public static string Host = nameof(Host).ToPascalCase(CultureInfo.CurrentCulture);
+
+        [Display(Name = "Database Name")]
+        public static string DatabaseName = nameof(DatabaseName).ToPascalCase(CultureInfo.CurrentCulture);
+        
+        public static string Username = nameof(Username).ToPascalCase(CultureInfo.CurrentCulture);
+        
+        public static string Password = nameof(Password).ToPascalCase(CultureInfo.CurrentCulture);
+
+        [Display(Name = "Port Number")]
+        public static string PortNumber = nameof(PortNumber).ToPascalCase(CultureInfo.CurrentCulture);
+        
+        public static string Schema = nameof(Schema).ToPascalCase(CultureInfo.CurrentCulture);
+
+        public static string ToDisplayName(this string propertyName)
+        {
+            var prop = typeof(CommonConfigurationNames).GetField(propertyName);
+
+            if (prop != null)
+            {
+                var attr = prop.GetAttribute<DisplayAttribute>();
+                if (attr != null) return attr.Name;
+            }
+
+            return propertyName;
+        }
     }
 }
---
+++ test/unit/Connector.Common.Test/SqlGenerationTests.cs
@@ -0,0 +1,19 @@
+﻿using CluedIn.Connector.Common.Configurations;
+using Xunit;
+
+namespace CluedIn.Connector.Common.Unit.Tests
+{
+    public class CommonConfigurationNamesTests
+    {
+        [Fact]
+        public void ToDisplayNameWorks()
+        {
+            Assert.Equal("Host", CommonConfigurationNames.Host.ToDisplayName());
+            Assert.Equal("Database Name", CommonConfigurationNames.DatabaseName.ToDisplayName());
+            Assert.Equal("Username", CommonConfigurationNames.Username.ToDisplayName());
+            Assert.Equal("Password", CommonConfigurationNames.Password.ToDisplayName());
+            Assert.Equal("Port Number", CommonConfigurationNames.PortNumber.ToDisplayName());
+            Assert.Equal("Schema", CommonConfigurationNames.Schema.ToDisplayName());
+        }
+    }
+}
