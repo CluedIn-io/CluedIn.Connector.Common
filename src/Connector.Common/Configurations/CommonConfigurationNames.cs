38ed67f7d4c71e2c216f3857a181b997f7d5ee7b
Ben Fidge <ben.fidge@btopenworld.com>
2022-01-24 16:45:19 +0700

    fix: Using cancelCase instead of PascalCase!

--- src/Connector.Common/Configurations/CommonConfigurationNames.cs
+++ src/Connector.Common/Configurations/CommonConfigurationNames.cs
@@ -7,19 +7,19 @@ namespace CluedIn.Connector.Common.Configurations
 
     public static class CommonConfigurationNames
     {
-        public static string Host = nameof(Host).ToPascalCase(CultureInfo.CurrentCulture);
+        public static string Host = nameof(Host).ToCamelCase(CultureInfo.CurrentCulture);
 
         [Display(Name = "Database Name")]
-        public static string DatabaseName = nameof(DatabaseName).ToPascalCase(CultureInfo.CurrentCulture);
+        public static string DatabaseName = nameof(DatabaseName).ToCamelCase(CultureInfo.CurrentCulture);
         
-        public static string Username = nameof(Username).ToPascalCase(CultureInfo.CurrentCulture);
+        public static string Username = nameof(Username).ToCamelCase(CultureInfo.CurrentCulture);
         
-        public static string Password = nameof(Password).ToPascalCase(CultureInfo.CurrentCulture);
+        public static string Password = nameof(Password).ToCamelCase(CultureInfo.CurrentCulture);
 
         [Display(Name = "Port Number")]
-        public static string PortNumber = nameof(PortNumber).ToPascalCase(CultureInfo.CurrentCulture);
+        public static string PortNumber = nameof(PortNumber).ToCamelCase(CultureInfo.CurrentCulture);
         
-        public static string Schema = nameof(Schema).ToPascalCase(CultureInfo.CurrentCulture);
+        public static string Schema = nameof(Schema).ToCamelCase(CultureInfo.CurrentCulture);
 
         public static string ToDisplayName(this string propertyName)
         {
