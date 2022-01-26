f0ef639aeb594ddd30187317222d7c5745b41986
Dmytro Lohvynenko <dmytro.lohvynenko@eleks.com>
2022-01-26 19:53:06 +0200

    Add Caching feature

---
+++ src/Connector.Common/Caching/InMemoryCachingService.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CluedIn.Connector.Common.Caching
+{
+    public class InMemoryCachingService<TItem, TConfiguration> : ICachingService<TItem, TConfiguration>
+        where TConfiguration : class
+    {
+        private List<KeyValuePair<TItem, TConfiguration>> _storage;
+
+        public InMemoryCachingService()
+        {
+            _storage = new List<KeyValuePair<TItem, TConfiguration>>();
+        }
+
+        public Task AddItem(TItem item, TConfiguration configuration)
+        {
+            _storage.Add(new KeyValuePair<TItem, TConfiguration>(item, configuration));
+
+            return Task.CompletedTask;
+        }
+
+        public Task Clear()
+        {
+            _storage.Clear();
+
+            return Task.CompletedTask;
+        }
+
+        public Task Clear(TConfiguration configuration)
+        {
+            _storage = _storage.Where(x => !x.Value.Equals(configuration)).ToList();
+
+            return Task.CompletedTask;
+        }
+
+        public Task<int> Count()
+        {
+            return Task.FromResult(_storage.Count);
+        }
+
+        public Task<IQueryable<KeyValuePair<TItem, TConfiguration>>> GetItems()
+        {
+            return Task.FromResult(_storage.AsQueryable());
+        }
+    }
+}
