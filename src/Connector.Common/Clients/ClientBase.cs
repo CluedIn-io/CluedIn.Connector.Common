146b2aa7c179592230ff0d1ae0517a600a368816
Oleksandr Povar <1074182+zvirja@users.noreply.github.com>
2022-11-22 16:25:36 +0100

    feat: Allow to specify schema (#21)

--- src/Connector.Common/Clients/ClientBase.cs
+++ src/Connector.Common/Clients/ClientBase.cs
@@ -58,24 +58,30 @@ namespace CluedIn.Connector.Common.Clients
             return connection;
         }
 
-        public virtual async Task<DataTable> GetTableColumns(IDictionary<string, object> config, string tableName)
+        public virtual async Task<DataTable> GetTableColumns(IDictionary<string, object> config, string tableName, string schema = null)
         {
-            return await GetRestrictedSchema(config, "Columns", tableName);
+            return await GetRestrictedSchema(config, "Columns", tableName: tableName, schema: schema);
         }
 
-        public virtual async Task<DataTable> GetTables(IDictionary<string, object> config, string name = null)
+        public virtual async Task<DataTable> GetTables(IDictionary<string, object> config, string name = null, string schema = null)
         {
-            return await GetRestrictedSchema(config, "Tables", name);
+            return await GetRestrictedSchema(config, "Tables", tableName: name, schema: schema);
         }
 
-        protected virtual async Task<DataTable> GetRestrictedSchema(IDictionary<string, object> config,
-            string collectionName, string restrictionValue = null)
+        protected virtual async Task<DataTable> GetRestrictedSchema(
+            IDictionary<string, object> config,
+            string collectionName,
+            string tableName = null,
+            string schema = null)
         {
             await using var connection = await GetConnection(config);
-            if (string.IsNullOrWhiteSpace(restrictionValue)) return connection.GetSchema(collectionName);
+            if (string.IsNullOrWhiteSpace(tableName))
+            {
+                tableName = null;
+            }
 
-            var restrictions = new string[4];
-            restrictions[2] = restrictionValue;
+            // Read more about syntax of restrictions here: https://learn.microsoft.com/en-us/dotnet/framework/data/adonet/schema-restrictions
+            var restrictions = new[] { null, schema, tableName, null };
             return connection.GetSchema(collectionName, restrictions);
         }
 
